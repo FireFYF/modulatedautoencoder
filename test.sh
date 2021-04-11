87349042b2d37b186e33f9f9df92c979bc863ad4
FireFYF <35377534+FireFYF@users.noreply.github.com>
2021-04-11 16:10:44 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=0 python mae.py --num_filters 192 192 192 --filters_offset 0 0 0 --model_ID 0 --condition 128 --condition_norm 2048.0 --checkpoint_dir /models/mae/ --inputPath /dataset/ --evaluation_name mae evaluate
+CUDA_VISIBLE_DEVICES=0 python mae.py --num_filters 192 192 192 --filters_offset 0 0 0 --lambda 128 512 2048 --model_ID 0 --condition 128 --condition_norm 2048.0 --checkpoint_dir /models/mae/ --inputPath /dataset/ --evaluation_name mae evaluate
 
 
 
